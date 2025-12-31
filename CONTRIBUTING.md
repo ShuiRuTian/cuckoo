2d3d6b363971584e90ec5f3c299a2f164fa70569
ShuiRuTian <158983297@qq.com>
2026-01-01 00:12:52 +0800

    init

---
+++ CONTRIBUTING.md
@@ -0,0 +1,4 @@
+## Setup environment
+
+1. Because we use aws-lc-sys, you need to install [cmake](https://cmake.org/) and [nasm](https://www.nasm.us/)
+
---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,23 @@
+use hudsucker::Proxy;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+
+    Proxy::builder().with_addr("127.0.0.1:8080")
+    .with_ca(ca)
+    .with_http_connector(connector)
+    .with_http_handler(http_handler)
+    .build();
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
